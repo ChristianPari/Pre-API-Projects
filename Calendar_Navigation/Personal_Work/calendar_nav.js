9275d914038b2d249561a0210b78252f87c40778
Christian Pari <ChristianPari@outlook.com>
2020-04-09 09:33:07 -0400

    Front-end/Back-end done; User can select date then reset via on-click button

--- Calendar_Navigation/Personal_Work/calendar_nav.js
+++ Calendar_Navigation/Personal_Work/calendar_nav.js
@@ -9,7 +9,7 @@
 // GLOBAL DATA / VARIABLES
 let calendarData = {
 
-    years: 2030,
+    years: 2020,
     months: 12,
     monthsObj: [{ 1: 31 }, { 2: 28 }, { 3: 31 }, { 4: 30 }, { 5: 31 }, { 6: 30 }, { 7: 31 }, { 8: 31 }, { 9: 30 }, { 10: 31 }, { 11: 30 }, { 12: 31 }]
 
@@ -22,29 +22,64 @@ intialElms();
 // CREATE HTML ELEMENTS
 function intialElms() {
 
-    // HEADING TO DISPLAY DATE INFO
-    let dateDisplay = createHeading({ text: `No date selected`, id: `dateHead`, size: 1 });
+    if (document.getElementById(`restartButton`) == null) {
 
-    // CREATE SELECTS
-    let yearSelect = createSelect({ id: `yearSelect`, class: `calendarSelects`, defOp: `Select A Year`, defOpID: `yearDef`, onChangeFunc: modifyData });
-    // let monthSelect = createSelect({ id: `monthSelect`, class: `calendarSelects`, defOp: `Select A Month`, defOpID: `monthDef`, onChangeFunc: modifyData });
-    // let daySelect = createSelect({ id: `daySelect`, class: `calendarSelects`, defOp: `Select A Day`, defOpID: `dayDef`, onChangeFunc: modifyData });
+        // HEADING TO DISPLAY DATE INFO
+        let dateDisplay = createHeading({ text: `No date selected`, id: `dateHead`, size: 1 });
+        dateDisplay.style.display = `none`;
 
-    document.body.appendChild(dateDisplay);
-    document.body.appendChild(yearSelect);
-    // document.body.appendChild(monthSelect);
-    // document.body.appendChild(daySelect);
+        // CREATE BUTTON
+        let beginButton = createButton({ id: `beginButton`, text: `Click to select a date!`, onClickFunc: showSelects });
 
-    yearSelectOptions();
+        document.body.appendChild(dateDisplay);
+        document.body.appendChild(beginButton);
+
+    } else {
+
+        document.body.innerHTML = ``;
+
+        // HEADING TO DISPLAY DATE INFO
+        let dateDisplay = createHeading({ text: `No date selected`, id: `dateHead`, size: 1 });
+        dateDisplay.style.display = `none`;
+
+        // CREATE BUTTON
+        let beginButton = createButton({ id: `beginButton`, text: `Click to select a date!`, onClickFunc: showSelects });
+
+        document.body.appendChild(dateDisplay);
+        document.body.appendChild(beginButton);
+
+    }
+
 
 }
 
 // FUNCTIONS TO UPDATE THE PROGRAM
+function dateDisplay() {
+
+    let regex = new RegExp(/\d{4}\d{2}\d{2}/);
+
+    if (regex.test(document.getElementById(`dateHead`).innerHTML)) {
+
+        document.getElementById(`dateHead`).innerHTML = document.getElementById(`dateHead`).innerHTML.replace(/\d{4}-\d{2}-\d{2}/)
+
+    }
+
+
+}
+
+function showSelects() {
+
+    let yearSelect = createSelect({ id: `yearSelect`, class: `calendarSelects`, defOp: `Select A Year`, defOpID: `yearDef`, onChangeFunc: modifyData });
+    document.body.appendChild(yearSelect);
+    yearSelectOptions();
+    document.getElementById(`beginButton`).style.display = `none`;
+
+}
 
 function yearSelectOptions() {
 
     let a = calendarData.years;
-    while (a >= 1) {
+    while (a >= 1920) {
 
         let option = document.createElement(`option`);
 
@@ -131,34 +166,34 @@ function modifyData() {
 
         let monthSelect = createSelect({ id: `monthSelect`, class: `calendarSelects`, defOp: `Select A Month`, defOpID: `monthDef`, onChangeFunc: modifyData });
 
-        // if (document.getElementById(`monthSelect`) != null) {
-
-        //     document.body.removeChild(`monthSelect`);
-
-        //     if (document.getElementById(`daySelect`) != null) {
-
-        //         document.body.removeChild(`monthSelect`);
-
-        //     }
-
-        // }
-
         document.body.appendChild(monthSelect);
         monthSelectOptionsByNum(data.value);
+        document.getElementById(`yearSelect`).style.display = `none`;
+        document.getElementById(`dateHead`).innerHTML = data.value;
 
     } else if (data.id == `monthSelect`) {
 
         let daySelect = createSelect({ id: `daySelect`, class: `calendarSelects`, defOp: `Select A Day`, defOpID: `dayDef`, onChangeFunc: modifyData });
 
-        // if (document.getElementById(`daySelect`) != null) {
-
-        //     document.body.removeChild(`daySelect`);
-
-        // }
-
         document.body.appendChild(daySelect);
         let month = data.value;
         daysSelectOptions(month);
+        document.getElementById(`monthSelect`).style.display = `none`;
+        if (data.value < 10) {
+            newVal = `/0${data.value}`;
+        }
+        document.getElementById(`dateHead`).innerHTML += newVal;
+
+    } else if (data.id == `daySelect`) {
+
+        let restartButton = createButton({ id: `restartButton`, text: `Click to select a new date!`, onClickFunc: intialElms });
+        if (data.value < 10) {
+            newVal = `0${data.value}`;
+        }
+        document.getElementById(`dateHead`).innerHTML += newVal;
+        document.getElementById(`daySelect`).style.display = `none`;
+        document.body.appendChild(restartButton);
+        document.getElementById(`dateHead`).style.display = `initial`;
 
     }
 
@@ -176,6 +211,24 @@ function createHeading(headingObj) {
     heading.id = headingObj.id != undefined && document.getElementById(headingObj.id) == null ? headingObj.id : `>> No ID <<`;
 
     return heading
+
+};
+
+function createButton(buttonObj) {
+
+    // id, class, text, onClickFunc
+
+    let button = document.createElement(`button`);
+
+    button.id = buttonObj.id != undefined && document.getElementById(buttonObj.id) == null ? buttonObj.id : `>> No ID <<`;
+
+    button.className = buttonObj.class != undefined ? buttonObj.class : ``;
+
+    button.innerHTML = typeof buttonObj.text == `string` ? buttonObj.text : `>> No Text <<`;
+
+    button.onclick = buttonObj.onClickFunc != undefined && typeof buttonObj.onClickFunc == `function` ? buttonObj.onClickFunc : `>> No function <<`;
+
+    return button
 
 };
 
